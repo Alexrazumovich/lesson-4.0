2d5219a66be82f47ec26eed3eb37f26fe03c677e
Alexrazumovich <alexrazumovich@gmail.com>
2024-07-13 15:03:40 +0300

    add function f1 in main

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
-
-print("Я конфликтую")
-
-for i in range(8):
-    print("word")
-a,b=9,8
-if a>b:
-    print("a больше b")
-elif b>a:
-    print("b больше a")
-else:
-    print("a равно b")
-a=1
-
+def f1(a,b):
+    return a+b
+a=5
+b=4
+print(f1(a,b))
